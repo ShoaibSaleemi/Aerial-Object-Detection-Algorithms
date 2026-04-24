ddd68250e2f0b30d7b35e932504be9d86252deba
ShoaibSaleemi <shoaib_saleemi@hotmail.com>
2026-04-24 17:41:16 +0200

    Add training script for YOLO model

---
+++ train_yolo.py
@@ -0,0 +1,20 @@
+from ultralytics import YOLO
+
+if __name__ == "__main__":
+    # 1. Load a model
+    # model = YOLO("yolov8n.yaml")  # build a new model from YAML (from scratch, not pretrained)
+    # model = YOLO("yolov8n.yaml").load("yolo8n.pt")  # build from YAML and transfer weights
+    model = YOLO("yolo26n.pt")  # load a pretrained model
+
+    # 2. Train the model, https://docs.ultralytics.com/modes/train/#musgd-optimizer
+    results = model.train(
+        data="C:/Users/shoai/project/thesis/data.yaml",
+        epochs=50, # pass over the entire dataset, affect training duration and model performance
+        imgsz=640, # input image size is 1024, but resized to 640 for training
+        batch=16,
+        workers=0, # avoid Windows shared-memory mapping failures in multi-worker dataloading
+        save=True, # save the training checkpoints
+        save_period=10, # frequency of saving a checkpoint, specified in epochs
+        optimizer="SGD", # We are using Stochastic Gradient Descent (SGD) optimizer
+        resume=False, # resume training from the latest saved checkpoint
+    )
